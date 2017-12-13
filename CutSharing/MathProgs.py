f3dbe147e21ba7b40ed4160cbfba2859bf180dd2
Daniel Duque <danielduque@u.northwestern.edu>
2017-12-13 07:02:47 -0600

    Changed num of threads to 4.

--- CutSharing/MathProgs.py
+++ CutSharing/MathProgs.py
@@ -41,7 +41,7 @@ class StageProblem():
         self.cut_pool = CutPool(stage)
         
         self.model.params.OutputFlag = 0
-        self.model.params.Threads = 8
+        self.model.params.Threads = 4
         #Add oracle var and include it in the objective
         self.cx = self.model.getObjective() #Get objective before adding oracle variable
         if last_stage ==False:
