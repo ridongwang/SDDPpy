958f069ef99f8736c2349433eed360336a172737
Daniel Duque <danielduque@u.northwestern.edu>
2018-06-28 16:00:45 -0500

    Running exp for out-of-sample with iter = 100, regardless of the upper bound.

--- HydroExamples/Hydro_ARx_ESS.py
+++ HydroExamples/Hydro_ARx_ESS.py
@@ -189,7 +189,7 @@ if __name__ == '__main__':
             instance_name = "Hydro_R%i_AR%i_T%i_I%i_ESS" % (nr, lag, T, CutSharing.options['max_iter'])
             Rmatrix = hydro_instance.ar_matrices
             RHSnoise_density = hydro_instance.RHS_noise[0:nr]
-            for N_training in [15,30,90]:#[2,3,5,10,20,30]:
+            for N_training in [30]:#[2,3,5,10,20,30]:
                 #Reset experiment design stream 
                 reset_experiment_desing_gen()
                 train_indeces = set(experiment_desing_gen.choice(range(len(RHSnoise_density[0])),size=N_training, replace = False))
@@ -229,7 +229,7 @@ if __name__ == '__main__':
                 prices = [10+round(5*np.sin(x),2) for x in range(0,T)]
                 
                 
-                CutSharing.options['max_iter'] = 300
+                CutSharing.options['max_iter'] = 100
                 '''
                 Expected value risk measure
                 '''
@@ -306,8 +306,8 @@ if __name__ == '__main__':
                 sim_results = list()
                 
                 #for rr in [b*(10**c) for c in [0] for b in [10]]:
-                #for rr in [b*(10**c) for c in [-3,-2,-1,-0,1,2] for b in [1,1.5,2,3,4,5,6,7,8,9]]:
-                for rr in r_lbs:
+                for rr in [b*(10**c) for c in [-3,-2,-1,-0,1,2] for b in [1,1.5,2,3,4,5,6,7,8,9]]:
+                #for rr in r_lbs:
                     print('DRO Dual Wasserstein Dynamic r = %10.4e' %(rr))
                     algo = SDDP(T, model_builder, random_builder, risk_measure = DistRobustWasserstein , norm = 1 , radius = rr)
                     lbs = algo.run(instance_name=instance_name, dynamic_sampling=True)
@@ -316,7 +316,7 @@ if __name__ == '__main__':
                     sim_results.append(sim_result)
                     del(algo)
                 sim_results_com.append(sim_results)
-                #plot_sim_results(sim_results, hydro_path+'/Output/%s_WassersteinDS.pdf' %(instance_name), len(valley_chain[0].inflows))
+                plot_sim_results(sim_results, hydro_path+'/Output/%s_WassersteinDS.pdf' %(instance_name), len(valley_chain[0].inflows))
                  
                 #plot_lbs(lbs_static, lbs_dynamic, len(valley_chain[0].inflows), r_lbs,  hydro_path+'/Output/%s_lbs_' %(instance_name))
                  
