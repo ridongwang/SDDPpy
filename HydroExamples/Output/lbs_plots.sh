a3575938b26805e075a846b4009117240236dbfa
Daniel Duque <danielduque@u.northwestern.edu>
2018-07-12 09:40:44 -0500

    Add run file for DW Dual (ours)

--- HydroExamples/Output/lbs_plots.sh
+++ HydroExamples/Output/lbs_plots.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python /Users/dduque/Dropbox/WORKSPACE/SDDP/OutputAnalysis/SimulationAnalysis.py DisceteWassersteinSingleCut/Hydro_R10_AR1_T12_N5_I100ESS_SC_DW_10.000000_DS_LBS.pickle DisceteWassersteinSingleCut/Hydro_R10_AR1_T12_N5_I100ESS_SC_DW_10.000000_ES_LBS.pickle  --exp_file=Hydro_R10_AR1_T12_N5 --path_to_files=/Users/dduque/Dropbox/WORKSPACE/SDDP/HydroExamples/Output/ --plot_type=LBS
+python /Users/dduque/Dropbox/WORKSPACE/SDDP/OutputAnalysis/SimulationAnalysis.py DisceteWassersteinSingleCut/Hydro_R10_AR1_T12_N5_I1000ESS_SC_DW_10.000000_DS_LBS.pickle DisceteWassersteinSingleCut/Hydro_R10_AR1_T12_N5_I1000ESS_SC_DW_10.000000_ES_LBS.pickle DW_Dual/Hydro_R10_AR1_T12_N5_I300ESS_MC_DW_10.000000_DS_LBS.pickle DW_Dual/Hydro_R10_AR1_T12_N5_I300ESS_MC_DW_10.000000_ES_LBS.pickle  --exp_file=Hydro_R10_AR1_T12_N5 --path_to_files=/Users/dduque/Dropbox/WORKSPACE/SDDP/HydroExamples/Output/ --plot_type=LBS
