29a03ff80089ab7a4f30cfa91dfb9d826d2d39a5
Daniel Duque <danielduque@u.northwestern.edu>
2018-10-18 17:02:06 -0500

    Fix bug in primal approach to submit the run

--- HydroExamples/Hydro_ARx_DW_Phlipott.py
+++ HydroExamples/Hydro_ARx_DW_Phlipott.py
@@ -20,8 +20,8 @@ if __name__ == '__main__':
     load_algorithm_options()
     T, model_builder, random_builder, rnd_container_data, rnd_container_oos, r_dro, instance_name =  load_hydro_data('PRIMAL' , 'DW')
     #options['cut_selector'] = SLACK_BASED_CUT_SELECTOR
-    algo = SDDP(T, model_builder, random_builder, risk_measure = DistRobust, dro_inner_solver = DiscreteWassersteinInnerSolver,\
-                dro_solver_params = {'norm': 1 , 'radius':r_dro , 'data_random_container':rnd_container_data})
+    algo = SDDP(T, model_builder, random_builder, risk_measure = DistRobust, dro_inner_solver = DiscreteWassersteinInnerSolver,  radius = r_dro, norm=1, data_random_container=rnd_container_data)
+
     lbs = algo.run(instance_name=instance_name, dynamic_sampling=options['dynamic_sampling'])
     
     save_path = hydro_path+'/Output/DW_Primal/%s_LBS.pickle' %(instance_name)
