f6c814d5c0da88580bb9424a389017aafffb1e38
Daniel Duque <danielduque@u.northwestern.edu>
2018-10-25 18:06:28 -0500

    Hydro model with extreamly high variance

--- CutSharing/SDDP_utils.py
+++ CutSharing/SDDP_utils.py
@@ -23,8 +23,12 @@ def report_stats(out_of_sample):
         out_of_sample (list of float): out-of sample results
     '''
     oos = np.array(out_of_sample)
+    p20 = np.percentile(oos,20)
+    p80 = np.percentile(oos,80)
     p10 = np.percentile(oos,10)
     p90 = np.percentile(oos,90)
+    p5 = np.percentile(oos,5)
+    p95 = np.percentile(oos,95)
     o_median = np.median(oos)
     o_mean = np.mean(oos)
     o_sd = np.std(oos)
@@ -32,4 +36,6 @@ def report_stats(out_of_sample):
     sddp_log.info('Mean:   \t%10.2f' %(o_mean))
     sddp_log.info('Median: \t%10.2f' %(o_median))
     sddp_log.info('SD:     \t%10.2f' %(o_sd))
+    sddp_log.info('20-80: \t(%10.2f, %10.2f)' %(p20,p80))
     sddp_log.info('10-90: \t(%10.2f, %10.2f)' %(p10,p90))
+    sddp_log.info('05-95: \t(%10.2f, %10.2f)' %(p5,p95))
