f9058c8210d3d236dba7810a711b0b9629cc187b
Daniel Duque <danielduque@u.northwestern.edu>
2019-02-27 15:22:09 -0600

    Changed hydro model to have a pwl thermal cost, and bounds on the reservoirs only for the first and last stages. This added a lot of variablity while have a coherent inflow instance.

--- OutputAnalysis/SimulationAnalysis.py
+++ OutputAnalysis/SimulationAnalysis.py
@@ -455,10 +455,13 @@ if __name__ == '__main__':
         experiment_files = os.listdir(path_to_files)
         sim_results = []
         for f in experiment_files:
+            print(f)
+            print(file_n in f , f[-6:]=='pickle' , '%s_OOS' %(dw_sampling) in f ,  '%s_%s' %(cut_type,sampling) in f , 'Time%i_' %(max_time) in f , dw_sampling in f)
             if file_n in f and f[-6:]=='pickle' and '%s_OOS' %(dw_sampling) in f and  '%s_%s' %(cut_type,sampling) in f and 'Time%i_' %(max_time) in f and dw_sampling in f:
                 print(f)
                 new_sim = pickle.load(open('%s%s' %(path_to_files,f), 'rb'))
                 sim_results.append(new_sim)
+
         sp_file = 'Hydro_R10_AR1_T24_N%i_%i_I100001' %(n,n)
         sp_sim = pickle.load(open('%s%s%s%i%s' %('/Users/dduque/Dropbox/WORKSPACE/SDDP/HydroExamples/Output/DW_Dual/',sp_file,'_Time',max_time,'_SP_MC_ES_OOS.pickle'), 'rb'))
         
